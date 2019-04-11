82d92cc60947e9d3a2f19fb6b35c34773d018981
Ada Toydemir <toydemir@usc.edu>
2019-04-10 19:39:21 -0700

    debugged

--- audio_normalization.py
+++ audio_normalization.py
@@ -4,18 +4,41 @@ def normalizeAudio(labels):
 	max_length = 0
 	o_length = 0
 
-	for i in range(0, len(labels)-1):
+	for i in range(0, len(labels)):
 		#list of numpy arrays of variable size (audio)
-		x, y = labels[i].size()
+		x, y = labels[i].shape
 		if (y>max_length):
 			max_length = y #double check if it is y or x lMAO
 
 	new_labels = []
 
-	for i in range(0, len(labels)-1):
-		temp = np.zeros(o_length, max_length)
-		x, y = labels[i].size()
-		for j in range(0, x-1):
-			for k in range(0, y-1):
+	for i in range(0, len(labels)):
+		temp = np.zeros((x, max_length))
+		x, y = labels[i].shape
+		for j in range(0, x):
+			for k in range(0, y):
 				temp[j][k] = labels[i][j][k]
 		new_labels.append(temp)
+
+	return new_labels
+
+a = np.ones((3, 3))
+b = np.ones((3, 10))
+c = np.ones((3, 7))
+d = np.ones((3, 12))
+
+l = [a, b, c, d]
+
+for i in range(0, 4):
+	print(l[i].shape)
+
+print("done")
+
+ll = normalizeAudio(l)
+
+print("New stuff")
+
+for i in range(0, 4):
+	print(ll[i].shape)
+
+print("done")
